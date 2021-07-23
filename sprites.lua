d5d98475e3aa62f61086f0ec1a92a1571d9d0acf
Shiqing <shiqing-thu18@yandex.com>
2021-07-24 00:10:04 +0800

    Use original sprites instead of spritesheet

--- sprites.lua
+++ sprites.lua
@@ -1,126 +1,37 @@
--- Spritesheets
+local cache = {}
 
--- Array of sprite batches
-local batches = {}
-local firstBatch
-
--- Map from names to sprite batches and rectangles
--- {
---   batch = <SpriteBatch>,
---   sx = <number>, sy = <number>,
---   sw = <number>, sh = <number>,  -- source rectangle
---   tx = <number>, ty = <number>,  -- destination origin
---   w = <number>, h = <number>     -- canvas size, all in pixels
--- }
-local lookup = {}
-
-local loadCrunch = function (path)
-  local splitPath = function (path)
-    local p = #path
-    local q   -- p: last '/'; q: last '.' after p
-    -- ord('/') == 47, ord('.') == 46
-    while p >= 1 and path:byte(p) ~= 47 do
-      if path:byte(p) == 46 and q == nil then q = p end
-      p = p - 1
-    end
-    q = q or #path + 1
-    return path:sub(1, p), path:sub(p + 1, q - 1)
+local function texture(name)
+  local tex = cache[name]
+  if tex == nil then
+    tex = love.graphics.newImage('res/sprites/' .. name .. '.png')
+    cache[name] = tex
   end
-  local wd, name = splitPath(path)
-
-  local f, err = love.filesystem.read(path)
-  if f == nil then
-    error('Cannot load sprite sheet metadata ' .. path .. ' (' .. err .. ')')
-    return nil
-  end
-
-  local p = 1
-
-  local read_int16 = function ()
-    local l, h = f:byte(p, p + 1)
-    p = p + 2
-    local x = h * 256 + l
-    if x >= 32768 then x = x - 65536 end
-    return x
-  end
-  local read_str = function ()
-    local q = p
-    repeat
-      local ch = f:byte(p)
-      p = p + 1
-      if ch == 0 then break end
-    until false
-    return f:sub(q, p - 2)
-  end
-
-  local texCount = read_int16()
-  for texId = 1, texCount do
-    local texName = read_str()
-    local img = love.graphics.newImage(wd .. texName .. '.png')
-    local batch = love.graphics.newSpriteBatch(img, nil, 'stream')
-    img:setFilter('nearest', 'nearest')
-    batches[#batches + 1] = batch
-
-    local sprCount = read_int16()
-    for sprId = 1, sprCount do
-      local name = read_str()
-      local spr = {}
-      -- spr.batch = batch
-      spr.sx = read_int16()
-      spr.sy = read_int16()
-      spr.sw = read_int16()
-      spr.sh = read_int16()
-      spr.tx = -read_int16()
-      spr.ty = -read_int16()
-      spr.w = read_int16()
-      spr.h = read_int16()
-      spr.quad = love.graphics.newQuad(
-        spr.sx, spr.sy, spr.sw, spr.sh,
-        img:getPixelDimensions())
-      lookup[name] = spr
-    end
-    firstBatch = batch
-  end
+  return tex
 end
-
-loadCrunch('res/sprites.bin')
-
--- Drawing
 
 local function draw(name, x, y, w, h, layer, r, ox, oy)
   -- layer = layer or 0
   r = r or 0
   ox = ox or 0
   oy = oy or 0
-  local item = lookup[name]
-  local cw, ch = item.w, item.h
-  local tx, ty = item.tx, item.ty
+  local tex = texture(name)
+  local cw, ch = tex:getDimensions()
   local scalex, scaley = w / cw, h / ch
   -- Only one batch
-  firstBatch:add(item.quad,
+  love.graphics.draw(tex,
     x, y,
     r, scalex, scaley,
-    -tx + cw * ox, -ty + ch * oy)
+    cw * ox, ch * oy)
+end
+
+local function delete(name)
+  cache[name] = nil
 end
 
 -- Optimized for batch drawing
 local function drawMulti(name, posList, w, h, layer, r, ox, oy)
-  -- layer = layer or 0
-  r = r or 0
-  ox = ox or 0
-  oy = oy or 0
-  local item = lookup[name]
-  local cw, ch = item.w, item.h
-  local tx, ty = item.tx, item.ty
-  local scalex, scaley = w / cw, h / ch
-  ox = -tx + cw * ox
-  oy = -ty + ch * oy
   for i = 1, #posList, 2 do
-    -- Only one batch
-    firstBatch:add(item.quad,
-      posList[i], posList[i + 1],
-      r, scalex, scaley,
-      ox, oy)
+    draw(name, posList[i], posList[i + 1], w, h, layer, r, ox, oy)
   end
 end
 
@@ -129,13 +40,7 @@ local function rectangle(x, y, w, h)
 end
 
 local function tint(r, g, b, a)
-  --[[
-  for _, v in ipairs(batches) do
-    v:setColor(r, g, b, a)
-  end
-  ]]
-  -- Only one batch
-  firstBatch:setColor(r, g, b, a)
+  love.graphics.setColor(r, g, b, a)
 end
 
 local function delete(name)
@@ -143,11 +48,7 @@ local function delete(name)
 end
 
 local function flush()
-  love.graphics.setColor(1, 1, 1)
-  for _, v in ipairs(batches) do
-    love.graphics.draw(v, 0, 0)
-    v:clear()
-  end
+  -- No-op before spritesheets and batches are implemented
 end
 
 return {
