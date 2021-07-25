8e71ee2dfb41c9d73cd3d9d19ff48c2cc6d561d7
Shiqing <shiqing-thu18@yandex.com>
2021-07-25 12:12:03 +0800

    Update image and audio assets

--- audio_thread.lua
+++ audio_thread.lua
@@ -4,10 +4,10 @@ require 'love.timer'
 
 local loop = require('audio').loop
 local bgm, update = loop(
-  'res/audio/background_intro.ogg', 60 / 144 * 8,
-  'res/audio/background_loop.ogg', 60 / 144 * 176,
+  'res/audio/background_intro.ogg', 60 / 126 * 8,
+  'res/audio/background_loop.ogg', 60 / 126 * 168,
   1600 * 4)
-bgm:setVolume(0.5 ^ 1.5)  -- -9 dB
+bgm:setVolume(0.5 ^ 1.75)  -- -10.5 dB
 
 while true do
   love.timer.sleep(0.5)
